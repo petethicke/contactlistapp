2d8cabe0640e830207551e8a22c54f8e60a0842e
Peter Thicke <peterbthicke@gmail.com>
2015-04-06 19:15:44 -0700

    Added CLI prompt

--- contact_list.rb
+++ contact_list.rb
@@ -1,2 +1,11 @@
+
+puts'Here is a list of available commands: 
+    new  - Create a new contact
+    list - List all contacts
+    show - Show a contact
+    find - Find a contact'
+
+
+
 require_relative 'contact'
 require_relative 'contact_database'
